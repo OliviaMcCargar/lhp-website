b44e7dffb68ce21d206987c3ecc82d262910e8c7
Olivia Kelly <oliviamccargar@gmail.com>
2025-05-25 15:00:34 -0400

    wagtail setup

---
+++ website/website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0p0^fk^3*=yfxxh=9a-0tf&vcy&kz4p%o&lxa63$11-k6!g#(-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
