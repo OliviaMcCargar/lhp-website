fe7c79b022dae475559e73ff25ad12ac77609d5e
Olivia Kelly <oliviamccargar@gmail.com>
2025-05-25 17:47:36 -0400

    switched database to postgres

--- website/entrypoint.sh
+++ website/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput --clear
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py makemigrations --settings=website.settings.dev
+python manage.py migrate --settings=website.settings.dev
+python manage.py collectstatic --noinput --clear --settings=website.settings.dev
 
 exec "$@"
