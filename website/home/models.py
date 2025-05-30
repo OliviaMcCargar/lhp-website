cd6130ea678b17ae6cc4ff43fb1d6edfb3626ff9
Olivia Kelly <oliviamccargar@gmail.com>
2025-05-30 14:37:14 -0400

    refined footer template, fixed migration issues

--- website/home/models.py
+++ website/home/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
-from wagtail.models import Page
+from wagtail.models import (
+    Page,
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+
 from wagtail.fields import RichTextField
 
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+
+from wagtail.snippets.models import register_snippet
 
 @register_setting
 class BrandSettings(BaseGenericSetting):
@@ -38,10 +47,38 @@ class SocialMediaSettings(BaseGenericSetting):
         ], heading="Social Media Settings")
     ]
 
+@register_snippet
+class FooterText(
+    DraftStateMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    TranslatableMixin,
+    models.Model,
+):
+
+    body = RichTextField()
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
+
 
 
 class HomePage(Page):
-    hero_image = models.ImageField(blank=True,upload_to=None, height_field=None, width_field=None, max_length=None)
+
     body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + ["hero_image","body"]
+    content_panels = Page.content_panels + ["body"]
